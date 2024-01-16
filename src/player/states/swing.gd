b7cf0f1657cf4c60f55daf8c2f5955833ccdd7b0
Tai <taiseicee@gmail.com>
2024-01-16 16:02:12 -0500

    Created Basic States

---
+++ src/player/states/swing.gd
@@ -0,0 +1,17 @@
+extends State
+
+func enter_state():
+	super()
+	print("Entered: Swing State")
+
+func process_frame(delta: float):
+	super(delta)
+	return null
+
+func process_input(event: InputEvent):
+	super(event)
+	return null
+
+func process_physics(delta: float):
+	super(delta)
+	return null
