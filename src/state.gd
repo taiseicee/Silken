275513af536dbec259551043bbbd8874a74ed100
Tai <taiseicee@gmail.com>
2024-01-16 15:31:19 -0500

    Created State Template for State Manager

---
+++ src/state.gd
@@ -0,0 +1,21 @@
+# A template for the states the state manager manages
+class_name State
+extends Node
+
+var character: CharacterBody2D
+var movement_comp: Node
+
+func enter_state():
+	pass
+
+func exit_state():
+	pass
+
+func process_frame(_delta: float):
+	pass
+
+func process_input(_event: InputEvent):
+	pass
+
+func process_physics(_delta: float):
+	pass
