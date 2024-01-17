38b62bdd45447cbb76499ad92c183823d1e4e020
Tai <taiseicee@gmail.com>
2024-01-16 21:42:34 -0500

    Added Damping Pendulum Physics

--- src/player/movement_comp.gd
+++ src/player/movement_comp.gd
@@ -1,16 +1,53 @@
 extends Node
 
 var character: CharacterBody2D
-var key_manager: Node
+@onready
+var key_manager: Node = $key_manager
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var first_key: Key
 var second_key: Key
 
-func init(character: CharacterBody2D, key_manager: Node):
+var swing_point: Vector2
+var web_length: float
+var resonant_frequency: float
+var beta: float
+
+var angular_vel: float
+var angle: float
+
+@export
+var mass: float = 1
+@export
+var air_friction: float = 0.35
+@export
+var web_length_multiplier: float = 20
+
+func init(character: CharacterBody2D):
 	self.character = character
-	self.key_manager = key_manager
+
+func handle_gravity(delta: float):
+	character.velocity.y += gravity * delta
+
+func shoot():
+	var first_location: Vector2 = key_manager.get_key_location(first_key)
+	var second_location: Vector2 = key_manager.get_key_location(second_key)
+	var direction: Vector2 = second_location - first_location
+	web_length = (web_length_multiplier * direction).length()
+	swing_point = character.position + web_length_multiplier * direction
+	resonant_frequency = sqrt(gravity / web_length)
+	beta = air_friction / (2 * mass)
+	
+	angular_vel = 0
+	angle = Vector2.DOWN.angle_to(direction)
+
+func swing(delta: float):
+	angular_vel = angular_vel + (delta * -2 * beta * angular_vel - (resonant_frequency * resonant_frequency * sin(angle)))
+	angle = angle + delta * angular_vel
+	
+	character.position.y = swing_point.y + web_length * cos(angle) 
+	character.position.x = swing_point.x + web_length * sin(angle)
 
 func set_first_key(key: Key) -> bool:
 	print("First Key:")
@@ -40,3 +77,6 @@ func print_key(key: Key):
 	var location: Vector2 = key_manager.get_key_location(key)
 	if is_valid_key(key, location):
 		print("%c - %d - %v" % [key, key, location])
+
+func draw_web():
+	character.draw_line(character.position, character.to_local(swing_point), Color(1, 1, 1))
