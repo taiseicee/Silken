d5f4526342951760f64211478c0915c0be1943db
Tai <taiseicee@gmail.com>
2024-01-19 20:25:50 -0500

    Changed Visuals

--- src/levels/level.gd
+++ src/levels/level.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var player: RigidBody2D = $player
 @onready var web: Web = $web
+@onready var camera: Camera2D = $player/camera
 
 func _on_player_spawn_web(pivot_point: Vector2):
 	web.shoot_web(player, pivot_point)
@@ -10,4 +11,5 @@ func _on_player_swinging(pivot_point: Vector2, web_length: float):
 	web.connect_to_body(player, pivot_point, web_length)
 
 func _on_player_dismiss_web():
+	camera.offset = Vector2.ZERO
 	web.disconnect_from_body()
