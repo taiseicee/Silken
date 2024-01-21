ec0202559e2589d5d9b04ca7c209f8517ae3e2aa
Tai <taiseicee@gmail.com>
2024-01-21 00:16:14 -0500

    Detect TileMap Walls/Cliffs With RayCasts

--- src/enemies/parasite/movement_comp.gd
+++ src/enemies/parasite/movement_comp.gd
@@ -5,15 +5,37 @@ extends Node
 @onready var ray_cast_down: RayCast2D = $"../ray_cast_down"
 
 @export var speed_patrol: float = 200
-@export var slide_distance: float = 500
+@export var slide_distance: float = 200
 @export var max_descent_angle: float = 75 * PI/180
 @export var ray_cast_buffer: float = 10
 
 var direction: float = -1
-var target_point: Vector2 = Vector2.ZERO
+var target_position: Vector2 = Vector2.ZERO
 
+var set_target_location_depth: int = 0
 func set_target_location():
-	pass
+	set_target_location_depth += 1
+	if set_target_location_depth > 2:
+		return
+	target_position = Vector2(direction * slide_distance, 0)
+	ray_cast_forward.target_position = target_position
+	if ray_cast_forward.is_colliding():
+		print(character.name + " Found Wall")
+		direction *= -1
+		set_target_location()
+		return
+	
+	ray_cast_down.position = Vector2(direction * slide_distance, 0)
+	var down_target_position = Vector2(0, sin(max_descent_angle))
+	var multiplier = slide_distance / cos(max_descent_angle)
+	ray_cast_down.target_position = multiplier * down_target_position
+	
+	ray_cast_down.force_raycast_update()
+	if ray_cast_down.is_colliding():
+		return
+	print(character.name + " Will Fall %v" % [multiplier * down_target_position])
+	direction *= -1
+	set_target_location()
 
 func patrol():
 	# TODO: Implement this better
--- src/enemies/parasite/states/move_patrol.gd
+++ src/enemies/parasite/states/move_patrol.gd
@@ -3,6 +3,15 @@ extends State
 func enter_state():
 	super()
 	#print("Move Machine > Patrol State")
+	movement_comp.set_target_location()
 
+var a = false
 func process_physics(_delta: float):
-	movement_comp.patrol()
+	if not a && movement_comp.ray_cast_forward.get_collider():
+		print("collide")
+		a = true
+	
+	movement_comp.ray_cast_down.force_raycast_update()
+	if movement_comp.ray_cast_down.is_colliding():
+		print("collide")
+	#movement_comp.patrol()
