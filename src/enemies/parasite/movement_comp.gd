8482f456b6f522d8f534fa14f729eaf16125120d
Tai <taiseicee@gmail.com>
2024-01-20 19:58:06 -0500

    Setup RayCasts for Parasite Slide

--- src/enemies/parasite/movement_comp.gd
+++ src/enemies/parasite/movement_comp.gd
@@ -1,10 +1,19 @@
 extends Node
 
 @onready var character: CharacterBody2D = $".."
+@onready var ray_cast_forward: RayCast2D = $"../ray_cast_forward"
+@onready var ray_cast_down: RayCast2D = $"../ray_cast_down"
 
 @export var speed_patrol: float = 200
+@export var slide_distance: float = 500
+@export var max_descent_angle: float = 75 * PI/180
+@export var ray_cast_buffer: float = 10
 
 var direction: float = -1
+var target_point: Vector2 = Vector2.ZERO
+
+func set_target_location():
+	pass
 
 func patrol():
 	# TODO: Implement this better
