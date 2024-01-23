dedf9e401ba7e154166d27a408b842c46f1b62c8
Tai <taiseicee@gmail.com>
2024-01-23 02:04:05 -0500

    Added Another Chunk

--- src/levels/level_chunk.gd
+++ src/levels/level_chunk.gd
@@ -1,11 +1,2 @@
 extends Node2D
 class_name LevelChunk
-
-@export var up_left: PackedScene
-@export var up: PackedScene
-@export var up_right: PackedScene
-@export var right: PackedScene
-@export var down_right: PackedScene
-@export var down: PackedScene
-@export var down_left: PackedScene
-@export var left: PackedScene
