04e509ed34b3ca296ac7658c873c95100d197be7
Tai <taiseicee@gmail.com>
2024-01-26 15:20:18 -0500

    Added Thorns + Logic

--- src/levels/level_labyrinth.gd
+++ src/levels/level_labyrinth.gd
@@ -1,7 +1,13 @@
 extends "res://src/levels/level_chunk.gd"
 
+@export var thorn_damage = 10
+
 signal give_player_dash_tutorial
 
 func _on_area_2d_body_entered(body):
 	if body is Player:
 		give_player_dash_tutorial.emit()
+
+func _on_thorns_hit_box_body_entered(body):
+	if body is Player:
+		globals.add_player_health(-thorn_damage)
